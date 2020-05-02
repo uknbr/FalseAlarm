50ddc8e725f88c49738cb0fda0b6c7bdbcce7b55
uknbr <ukn_linux@yahoo.com.br>
2020-05-02 17:46:08 -0300

    Boinc - Help in the fight against COVID-19

---
+++ boinc/boinc.sh
@@ -0,0 +1,16 @@
+# Install
+aptitude search boinc
+sudo aptitude install boinc
+
+# Start manager - GUI
+boincmgr
+
+# CLI
+boinccmd --get_tasks
+boinccmd --get_state
+boinccmd --get_disk_usage
+boinccmd --acct_mgr info
+boinccmd --get_simple_gui_info
+
+# Config
+vim /var/lib/boinc-client/cc_config.xml
