010e1cf8d83fc559974216468516f448f82401ae
Pedro Pavan <pedro.abreupavan@amdocs.com>
2020-12-06 22:04:28 -0300

    [feature] temperature with mqtt notification

--- temperature/sensor.py
+++ temperature/sensor.py
@@ -3,11 +3,21 @@ from DHT11_Python import dht11
 import time
 import os
 import datetime
+import paho.mqtt.client as mqtt
 
 # DEBUG
 #from birdseye import eye
 #birdseye --host 0.0.0.0 --port 7010
 #@eye
+
+def update_mqtt(topic, value):
+    if mqtt_enable:
+        try:
+            client = mqtt.Client()
+            client.connect(mqtt_host, mqtt_port, 60)
+            client.publish(f"{mqtt_host}/{car_model}/{topic}", value, retain=True)
+        except Exception as e:
+            print(f"Failed to publish '{value}' on topic {topic}")
 
 def main():
 	GPIO.setwarnings(False)
@@ -27,6 +37,8 @@ def main():
 			if result.is_valid():
 				count += 1
 				print('[{0} | {1} | {2}] Temp={3:0.1f}ºC Humidity={4:0.1f}%'.format(count, a, now.strftime("%Y-%m-%d %H:%M:%S"), result.temperature, result.humidity))
+				update_mqtt(f"{mqtt_host}/sensor/temperature", result.temperature)
+				update_mqtt(f"{mqtt_host}/sensor/humidity", result.humidity)
 				break
 			else:
 				#print(f"Failed {a} to get reading. Try again!")
@@ -34,7 +46,11 @@ def main():
 					print('[{0} | {1} | {2}] Temp={3:0.1f}ºC Humidity={4:0.1f}%'.format(count, a, now.strftime("%Y-%m-%d %H:%M:%S"), 0, 0))
 			time.sleep(2)
 
-		time.sleep(int(os.getenv('INTERVAL')))
+		time.sleep(int(os.getenv("INTERVAL", 30)))
 
 if __name__ == '__main__':
+	mqtt_enable = eval(os.getenv("MQTT_ENABLE", "False"))
+	mqtt_host = os.getenv("MQTT_HOST", "localhost")
+	mqtt_port = int(os.getenv("MQTT_PORT", 1883))
+
 	main()
