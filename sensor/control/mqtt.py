fcb6a884c3347693b518dfb0153116eca0e76b57
Pedro Pavan <pedro.abreupavan@amdocs.com>
2020-12-13 19:11:12 -0300

    [fix] Identation space

--- sensor/control/mqtt.py
+++ sensor/control/mqtt.py
@@ -16,28 +16,28 @@ except:
     raise SystemExit("Variable was not defined!")
 
 def on_connect(client, userdata, flags, rc):
-        print(f"Connected with result code {rc}")
-        client.subscribe("florindabox/sensor/temperature",1)
-        client.subscribe("florindabox/sensor/humidity",1)
-        client.subscribe("florindabox/sensor/buzzer",1)
-        client.subscribe("florindabox/sensor/led",1)
+    print(f"Connected with result code {rc}")
+    client.subscribe("florindabox/sensor/temperature",1)
+    client.subscribe("florindabox/sensor/humidity",1)
+    client.subscribe("florindabox/sensor/buzzer",1)
+    client.subscribe("florindabox/sensor/led",1)
 
 def on_message(client, userdata, msg):
-        topic = msg.topic
-        message = msg.payload.decode("utf-8")
-        print(f"[Message] {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} = {message} ({topic})")
+    topic = msg.topic
+    message = msg.payload.decode("utf-8")
+    print(f"[Message] {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} = {message} ({topic})")
 
-        if topic == "florindabox/sensor/buzzer":
-            if message == "1":
-                beep_on()
-            if message == "0":
-                beep_off()
+    if topic == "florindabox/sensor/buzzer":
+        if message == "1":
+            beep_on()
+        if message == "0":
+            beep_off()
 
-        if topic == "florindabox/sensor/led":
-            if message == "1":
-                led_on()
-            if message == "0":
-                led_off()
+    if topic == "florindabox/sensor/led":
+        if message == "1":
+            led_on()
+        if message == "0":
+            led_off()
 
 if mqtt_enable:
     print(f"[System] MQTT is enabled")
