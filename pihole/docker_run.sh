bf243a243f74bca0373522a7fc553c242b0c0cbf
uknbr <ukn_linux@yahoo.com.br>
2021-05-16 11:33:18 -0300

    Get IP automatically

--- pihole/docker_run.sh
+++ pihole/docker_run.sh
@@ -2,6 +2,10 @@
 
 # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
 
+IP=$(hostname -I | tr ' ' '\n' | grep 192)
+#IP=192.168.15.36
+#    -v "$(pwd)/var-log/:/var/log/" \
+
 docker run -d \
     --name pihole \
     --net=host \
@@ -9,10 +13,9 @@ docker run -d \
     -p 80:80 \
     -p 443:443 \
     -e TZ="America/Sao_Paulo" \
-    -e ServerIP="192.168.0.108" \
+    -e ServerIP=${IP} \
     -v "$(pwd)/etc-pihole/:/etc/pihole/" \
     -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-    -v "$(pwd)/var-log/:/var/log/" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
     --restart=unless-stopped \
     pihole/pihole:latest
@@ -21,7 +24,7 @@ printf 'Starting up pihole container '
 for i in $(seq 1 20); do
     if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
         printf ' OK'
-        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: https://${ServerIP}/admin/"
+        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: http://${IP}/admin/"
         exit 0
     else
         sleep 3
@@ -32,4 +35,4 @@ for i in $(seq 1 20); do
         echo -e "\nTimed out waiting for Pi-hole start, consult check your container logs for more info (\`docker logs pihole\`)"
         exit 1
     fi
-done;
+done
