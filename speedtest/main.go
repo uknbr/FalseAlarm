15a9fa6ae7fd0a076b325a1af0bc09dfa7ea16f7
uknbr <ukn_linux@yahoo.com.br>
2022-01-20 18:07:43 -0300

    env variables as configuration

--- speedtest/main.go
+++ speedtest/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 	_ "time/tzdata"
 	"log"
@@ -9,13 +10,21 @@ import (
 	"github.com/influxdata/influxdb1-client/v2"
 )
 
+func getEnv(key, fallback string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        value = fallback
+    }
+    return value
+}
+
 func convertLatency(s time.Duration) float64 {
 	return float64(s / time.Millisecond)
 }
 
 func influxDBClient() client.Client {
 	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr:     "http://localhost:8086",
+		Addr:     "http://" + getEnv("INFLUX_HOST", "localhost") + ":" + getEnv("INFLUX_PORT", "8086"),
 	})
 	if err != nil {
 		log.Fatalln("Error: ", err)
@@ -23,16 +32,18 @@ func influxDBClient() client.Client {
 	return c
 }
 
-func addMetrics(c client.Client, l float64, d float64, u float64) {
+func addMetrics(l float64, d float64, u float64) {
+	var c client.Client = influxDBClient()
+
 	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "speedtest",
+		Database:  getEnv("INFLUX_SERIE", "speedtest"),
 		Precision: "s",
 	})
 	if err != nil {
 		log.Fatalln("Error: ", err)
 	}
 
-	loc, err := time.LoadLocation("America/Sao_Paulo")
+	loc, err := time.LoadLocation(getEnv("SPEEDTEST_TZ", "America/Sao_Paulo"))
 	if err != nil {
 		panic(err)
 	}
@@ -49,7 +60,7 @@ func addMetrics(c client.Client, l float64, d float64, u float64) {
 	}
 
 	point, err := client.NewPoint(
-		"speed_status",
+		"speedtest",
 		tags,
 		fields,
 		time.Now().In(loc),
@@ -67,8 +78,19 @@ func addMetrics(c client.Client, l float64, d float64, u float64) {
 }
 
 func main() {
-	fmt.Print("Initializing InfluxDB...\n")
-	c := influxDBClient()
+	fmt.Print("Looking for configuration...\n")
+	var enable_influx bool
+	env_enable_influx := getEnv("INFLUX_ENABLE", "false")
+	if env_enable_influx == "false" {
+		enable_influx = false
+	} else {
+		enable_influx = true
+	}
+
+	interval, err := time.ParseDuration(getEnv("SPEEDTEST_INTERVAL", "5m"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Print("Retrieving speedtest.net configuration...\n")
 	user, _ := speedtest.FetchUserInfo()
@@ -86,9 +108,11 @@ func main() {
 			s.UploadTest(false)
 
 			latency := convertLatency(s.Latency)
-			fmt.Printf("[%s] Latency: %f ms\tDownload: %f Mbit/s\tUpload: %f Mbit/s\n", time.Now().Format("Jan 02, 2006 15:04:05"), latency, s.DLSpeed, s.ULSpeed)
-			addMetrics(c, latency, s.DLSpeed, s.ULSpeed)
+			fmt.Printf("[%s] Latency: %f ms\tDownload: %f Mbit/s\tUpload: %f Mbit/s\n", time.Now().Format("Jan 09, 2020 15:04:05"), latency, s.DLSpeed, s.ULSpeed)
+			if enable_influx {
+				addMetrics(latency, s.DLSpeed, s.ULSpeed)
+			}
 		}
-		time.Sleep(time.Minute * 5)
+		time.Sleep(interval)
 	}
 }
