22c1f46a071df031389abb7b8e53c5f1a7933a1e
uknbr <ukn_linux@yahoo.com.br>
2022-01-21 23:54:29 -0300

    Multi-platform build

--- speedtest/build.sh
+++ speedtest/build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-archs=(arm)
+archs=(amd64 arm)
 mkdir -p ./bin
 
 for arch in ${archs[@]}
 do
     env GOOS=linux GOARCH=${arch} go build -o ./bin/speedtest_${arch}
-    docker build --build-arg ARCH=${arch} -t uknbr/florindabox-speedtest:latest .
-    docker push uknbr/florindabox-speedtest:latest
+    du -h ./bin/speedtest_${arch}
 done
+
+docker buildx build --platform linux/amd64,linux/arm -t uknbr/florindabox-speedtest --push .
+
+
+
+#wget https://github.com/docker/buildx/releases/download/v0.7.1/buildx-v0.7.1.linux-amd64
+#mkdir -p ~/.docker/cli-plugins/
+#mv buildx-v0.7.1.linux-amd64 ~/.docker/cli-plugins/docker-buildx
+#chmod +x ~/.docker/cli-plugins/docker-buildx
